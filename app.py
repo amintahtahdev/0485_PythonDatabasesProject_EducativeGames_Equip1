f00198c6dfa106227def4e655f892830d7c85b52
arnaumontsia <arnaucaballer@iesmontisa.org>
2026-05-19 08:33:53 +0200

    Resolts confictes al merge de app.py

--- app.py
+++ app.py
@@ -8,8 +8,23 @@ from extensions import db
 from config import SECRET_KEY
 from routes.auth import auth_bp
 from routes.game_routes import game_bp
+from routes.main_routes import main_bp
 
 
+def create_app():
+    app = Flask(__name__)
+    app.secret_key = SECRET_KEY
+    app.register_blueprint(auth_bp)
+    app.register_blueprint(game_bp)
+    app.register_blueprint(main_bp)
+    return app
+
+
+app = create_app()
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
 app = Flask(__name__)
 app.secret_key = 'clau_secreta_super_segura_per_educative_game'
 
